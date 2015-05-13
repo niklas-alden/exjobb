8d6a00f5bbe36a47e0ef101261a1941c3e2bd699
Niklas <niklas.alden1@gmail.com>
2015-05-13 18:52:32 +0200

    updated script for 15 bit gain

--- python/generate_lut.py
+++ python/generate_lut.py
@@ -1,28 +1,30 @@
 def tohex(val, nbits):
     return hex((val + (1 << nbits)) % (1 << nbits))
 
-gain_file = open('matlab_gain_lut_poly3.txt', 'r')
-g_lines = gain_file.readlines()
-fix_file = open('gain_lut_template.vhd', 'r')
-fix_lines = fix_file.readlines()
-ofile = open('gain_lut_py_poly3.vhd', 'w')
+def to_bin(val, nbit):
+    leading_zeros = '{0:0' + str(nbit) + 'b}'
+    return leading_zeros.format(val)
 
-for f_line in fix_lines:
-    ofile.write(f_line)
-    if 'case dB_max_c is' in f_line:
-        n = len(g_lines);
-        for line in reversed(g_lines):
-            g = int(float(line) * (2**15))
-            dB = str(tohex(n-82,8))[2:]
-            if len(dB) < 2:
-                dB = '0' + dB
-            gain = str(hex(g)[2:])
-            if gain == '8000':
-                gain = '7fff'
-            ofile.write('        when x"' + dB + '" => gain_n <= x"' + gain + '"; -- ' + str(n) + 'dB' + '\n')
-            n -= 1
+gain_file = open('matlab_gain_lut_poly1.txt', 'r')
+g_lines = gain_file.readlines()
+ofile = open('gain_lut_py_poly1_bin.vhd', 'w')
+
+limit = 62
+n = len(g_lines) - (82 - limit)
+for line in reversed(g_lines):
+    g = int(float(line) * (2**15))
+    dB = str(tohex(n-82,8))[2:]
+    if len(dB) < 2:
+        dB = '0' + dB
+    gain = to_bin(g, 15)
+    if gain == '1' + '0'*15:
+        gain = '1'*15
+    ofile.write('\t\twhen x"' + dB + '" => gain_n <= "' + gain + '"; -- ' + str(n) + 'dB' + '\n')
+    n -= 1
+    if n < 0:
+        break
     
 gain_file.close()
-fix_file.close()
+
 ofile.close()
 
